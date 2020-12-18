48684709835bf6b2de865b9bca18014331a6be2c
yamil esquivel <niisanyamil@gmail.com>
2020-12-17 21:39:03 -0300

    fix: readme

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,7 +1,7 @@
 //
 // This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
+//Author: yamilesquivel
 
 export const toRna = (dna) => {
   let rna = '';
