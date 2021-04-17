7a8b8e87a536fe736cd4476db5bc33cb1b1fdc39
yamil esquivel <niisanyamil@gmail.com>
2021-04-17 04:02:51 -0300

    space-age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -4,6 +4,18 @@
 //Github user: @yamilesquivel , Web : yamilesquivel.github.io
 
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+
+const planetFactor = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
+
+export const age = (planet, seconds) => {
+  return +((seconds) / (planetFactor[planet] * (365.25 * 24 * 60 * 60))).toFixed(2)
 };
