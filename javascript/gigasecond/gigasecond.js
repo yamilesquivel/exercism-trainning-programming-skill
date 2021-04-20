41ce15f16d339478aabe2b9f976885dd62f1528a
yamil esquivel <niisanyamil@gmail.com>
2021-04-20 05:22:11 -0300

    fix: bank-account

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,7 +1,8 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
+//Github user: @yamilesquivel , website : yamilesquivel.github.io
+
+
 export const gigasecond = (date) => {//take a date
   return new Date(date.getTime() + 10**12); //return a time plus a gigasecond
 };
